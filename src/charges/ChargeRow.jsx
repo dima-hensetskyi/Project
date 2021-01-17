8bb6f893ea420e4b985608ccadd7ce54102665f4
unknown <dimagena9@gmail.com>
2021-01-17 14:43:30 +0200

    task-3-sort-table-elements

--- src/charges/ChargeRow.jsx
+++ src/charges/ChargeRow.jsx
@@ -11,11 +11,11 @@ import './ChargeRow.css';
 import Icon from './Icon';
 
 const categoriesOptions = [
-  { value: 'food', label: 'Food' },
-  { value: 'clothes', label: 'Clothes' },
+  	{ value: 'food', label: 'Food' },
+  	{ value: 'clothes', label: 'Clothes' },
 	{ value: 'restaurant', label: 'Restaurant' },
 	{ value: 'utility bills', label: 'Utility bills' },
-  { value: 'pets', label: 'Pets' },
+  	{ value: 'pets', label: 'Pets' },
 ];
 
 function ChargeRow({id, category, description, date, money, onChargeChange, onSaveNewCharge, onCancelNewCharge}) {
@@ -26,38 +26,38 @@ function ChargeRow({id, category, description, date, money, onChargeChange, onSa
 					value={{value: category, label: category}}
 					onChange={(selectedOption) => onChargeChange({id, description, date, money, category: selectedOption.value})}
 					categoriesOptions={categoriesOptions}
-      	/>
+      			/>
 			</td>
 			<td>
 				<Form.Control
-				 type="text" 
-				 placeholder="Description" 
-				 onChange={({target}) => onChargeChange({id, description: target.value, date, money, category})} 
-				 value={description}/>
+					type="text" 
+					placeholder="Description" 
+					onChange={({target}) => onChargeChange({id, description: target.value, date, money, category})} 
+					value={description}/>
 				 </td>
 				 <td>
 				 <DayPickerInput
-        formatDate={formatDate}
-        parseDate={parseDate}
-				value={formatDate(date)}
-				onDayChange={day => onChargeChange({id, description, date: formatDate(day), money, category})}
-      	/>
-					</td>
+					formatDate={formatDate}
+					parseDate={parseDate}
+					value={formatDate(date)}
+					onDayChange={day => onChargeChange({id, description, date: formatDate(day), money, category})}
+      			/>
+			</td>
 			<td>
 				<Form.Control 
-				type="text" 
-				placeholder="Money" 
-				onChange={({target}) => onChargeChange({id, description, date, money: target.value, category})} 
-				value={money}/>
+					type="text" 
+					placeholder="Money" 
+					onChange={({target}) => onChargeChange({id, description, date, money: target.value, category})} 
+					value={money}/>
 			</td>
 			<td>
 				<div className="action-buttons">
 					<Icon iconName="save"
 						onClick={onSaveNewCharge}
-						/>
-							<Icon iconName="cancel"
+					/>
+					<Icon iconName="cancel"
 						onClick={onCancelNewCharge}
-						/>
+					/>
 				</div>
 			</td>
 		</tr>
--- src/charges/TableCharges.jsx
+++ src/charges/TableCharges.jsx
@@ -1,136 +1,182 @@
 import React, { useState } from 'react';
 
-import {Table} from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
-import MomentLocaleUtils, {
-	formatDate,
-	parseDate,
-  } from 'react-day-picker/moment';
+import MomentLocaleUtils, { formatDate, parseDate } from 'react-day-picker/moment';
 
 import { v4 as uuidv4 } from 'uuid';
 
 import ChargeRow from './ChargeRow';
 import './TableCharges.css';
 import Icon from './Icon';
+import { propTypes } from 'react-bootstrap/esm/Image';
+import _ from 'lodash';
 
 function TableCharges() {
-	const storedCharges = JSON.parse(localStorage.getItem("charges"));
+    const storedCharges = JSON.parse(localStorage.getItem('charges'));
 
-	const [charges, setCharges] = useState(storedCharges || []);
-	const [newCharge, setNewCharge] = useState(null);
+    const [charges, setCharges] = useState(storedCharges || []);
+    const [newCharge, setNewCharge] = useState(null);
     const [editableChargeId, setEditableChargeId] = useState();
-    
+    const [sort, setSort] = useState('desc');
+    const [sortField, setSortField] = useState('id');
+
     const headers = ['Category', 'Description', 'Date', 'Money', 'Action'];
 
     const handleAddNewCharge = () => {
-		setNewCharge({
-			id: uuidv4(),
-			category: '',
-			description : '',
-			date: formatDate(new Date()),
-			money: '$'
-		})
-	}; 
-
-	const handleSaveNewCharge = () => {
-		localStorage.setItem("charges", JSON.stringify([...charges, newCharge]));
-		setCharges([...charges, newCharge]);
-		setNewCharge(null);
-		setEditableChargeId(null);
-	}
-
-	const handleSaveEditableCharge = () => {
-		const updatedCharges = charges.map((charge) => {
-			if(charge.id === newCharge.id){
-				return newCharge;
-			}
-			return charge;
-		})
-
-		localStorage.setItem("charges", JSON.stringify(updatedCharges));
-
-		setCharges(updatedCharges);
-		setNewCharge(null);
-		setEditableChargeId(null);
-	}
-
-	const handleCancelNewCharge = () => {
-		setNewCharge(null);
-		setEditableChargeId(null);
-	}
-
-	const handleChargeChange = (charge) => {
-		setNewCharge(charge);
-	}
-
-	const handleEditCharge = (charge) => {
-		setEditableChargeId(charge.id);
-		setNewCharge(charge);
-	}
-
-	const handleDeleteCharge = (id) => {
-		const arrayCharges = charges.filter((charge) => charge.id !== id);
-
-		localStorage.setItem("charges", JSON.stringify(arrayCharges));
-
-		setCharges([...arrayCharges]);
-	}
-
-	const buildChargeRow = (charge) => {
-		return (
-			<tr key={charge.id}>
-				<td>{charge.category}</td>
-				<td>{charge.description}</td>
-				<td>{charge.date}</td>
-				<td>{charge.money}</td>
-				<td>	
-					<div className="action-buttons">
-					<Icon iconName="edit"
-						onClick={() => handleEditCharge(charge)}
-						/>
-					<Icon iconName="delete"
-						onClick={() => handleDeleteCharge(charge.id)}
-						/>
-					</div>
-				</td>
-			</tr>)
+        setNewCharge({
+            id: uuidv4(),
+            category: '',
+            description: '',
+            date: formatDate(new Date()),
+            money: '',
+        });
     }
 
+    const handleSaveNewCharge = () => {
+        localStorage.setItem('charges', JSON.stringify([...charges, newCharge]));
+        setCharges([...charges, newCharge]);
+        setNewCharge(null);
+        setEditableChargeId(null);
+    }
+
+    const handleSaveEditableCharge = () => {
+        const updatedCharges = charges.map((charge) => {
+            if (charge.id === newCharge.id) {
+                return newCharge;
+            }
+            return charge;
+        });
+
+        localStorage.setItem('charges', JSON.stringify(updatedCharges));
+
+        setCharges(updatedCharges);
+        setNewCharge(null);
+        setEditableChargeId(null);
+    }
+
+    const handleCancelNewCharge = () => {
+        setNewCharge(null);
+        setEditableChargeId(null);
+    }
+
+    const handleChargeChange = (charge) => {
+        setNewCharge(charge);
+    }
+
+    const handleEditCharge = (charge) => {
+        setEditableChargeId(charge.id);
+        setNewCharge(charge);
+    }
+
+    const handleDeleteCharge = (id) => {
+        const arrayCharges = charges.filter((charge) => charge.id !== id);
+
+        localStorage.setItem('charges', JSON.stringify(arrayCharges));
+
+        setCharges([...arrayCharges]);
+    }
+
+    const buildChargeRow = (charge) => {
+        return (
+            <tr key={charge.id}>
+                <td>{charge.category}</td>
+                <td>{charge.description}</td>
+                <td>{charge.date}</td>
+                <td>{charge.money}</td>
+                <td>
+                    <div className="action-buttons">
+                        <Icon iconName="edit" onClick={() => handleEditCharge(charge)} />
+                        <Icon iconName="delete" onClick={() => handleDeleteCharge(charge.id)} />
+                    </div>
+                </td>
+            </tr>
+        )
+    }
+
+    const onSort = (category) => {
+        const cloneData = charges.map((item) => {
+            if (typeof item.date === 'string' || typeof moneyFormat === 'string') {
+                const сorrectDateFormat = Date.parse(item.date);
+                const moneyFormat = item.money.replace(/\D/g, '');
+                return {
+                    ...item,
+                    money: Number(moneyFormat),
+                    date: сorrectDateFormat,
+                }
+            }
+            return item;
+        });
+
+        const sortType = sort === `desc` ? `asc` : `desc`;
+        setSort(sortType);
+        setSortField(category);
+        const orderedData = _.orderBy(cloneData, category.toLowerCase(), sortType);
+
+        const correctData = orderedData.map((item) => {
+            const date = new Date(item.date);
+
+            const correctDay = (date) =>
+                date.getDate() <= 9 ? `0${date.getDate()}` : `${date.getDate()}`;
+            const correctMounth = (date) =>
+                date.getMonth() <= 9 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+
+            const correctDateFormat = `${correctMounth(date)}/${correctDay(date)}/${date.getFullYear()}`;
+            return {
+                ...item,
+                money: item.money + '$',
+                date: correctDateFormat,
+            }
+        });
+        setCharges(correctData);
+    }
     return (
-		<div className="charge-table">
-		<div className="d-flex justify-content-end pb-3">
-			<Icon iconName="add"
-				size="big"
-				onClick={handleAddNewCharge}
-						/>
-		</div>
-        <Table striped bordered hover>   
-            <thead>
-                <tr> 
-                    {headers.map((header) => <th>{header}</th>)}
-                </tr>
-            </thead>
-            <tbody>
-                {charges.map((charge) => {
-                    if (editableChargeId === charge.id){
-                        return (<ChargeRow key={charge.id} {...newCharge} 
-                        onChargeChange={handleChargeChange} 
-                        onSaveNewCharge={handleSaveEditableCharge} 
-                        onCancelNewCharge={handleCancelNewCharge} 
-                                />)
-                    } else{
-                        return buildChargeRow(charge);
-                    }
-                })}
-                {newCharge && !editableChargeId &&
-                    (<ChargeRow key={newCharge.id} {...newCharge}
-                    onChargeChange={handleChargeChange}
-                    onSaveNewCharge={handleSaveNewCharge}
-                    onCancelNewCharge={handleCancelNewCharge}
-                    />)}
-            </tbody>
-        </Table>
-		</div>
+        <div className="charge-table">
+            <div className="d-flex justify-content-end pb-3">
+                <Icon iconName="add" size="big" onClick={handleAddNewCharge} />
+            </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        {headers.map((header, index) => (
+                            <th
+                                key={index}
+                                onClick={onSort.bind(null, `${header}`)}
+                                className={sortField === header ? 'sort' : 'notSorting'}
+                            >
+                                {header}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {charges.map((charge) => {
+                        if (editableChargeId === charge.id) {
+                            return (
+                                <ChargeRow
+                                    key={charge.id}
+                                    {...newCharge}
+                                    onChargeChange={handleChargeChange}
+                                    onSaveNewCharge={handleSaveEditableCharge}
+                                    onCancelNewCharge={handleCancelNewCharge}
+                                />
+                            );
+                        } else {
+                            return buildChargeRow(charge);
+                        }
+                    })}
+                    {newCharge && !editableChargeId && (
+                        <ChargeRow
+                            key={newCharge.id}
+                            {...newCharge}
+                            onChargeChange={handleChargeChange}
+                            onSaveNewCharge={handleSaveNewCharge}
+                            onCancelNewCharge={handleCancelNewCharge}
+                        />
+                    )}
+                </tbody>
+            </Table>
+        </div>
     );
 }
 
